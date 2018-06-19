6c8d1e0fd27773a4b8692182a28bc96649e1d9a6
Russell Strauss <rstrauss@agencyoasis.com>
2018-06-19 01:15:38 -0400

    Adding Angular 5 structure. Come back and add correct JavaScript structure because it is currently broken after importing from Browserify object structure.

---
+++ ng5/src/app/app.trail-map.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.trail-map.html',
+  styleUrls: ['./app.trail-map.scss']
+})
+
+export class AppComponent {
+  title = 'Smokey Mountain Interactive Trail Map';
+}
---
+++ ng5/src/assets/js/map.js
@@ -0,0 +1,214 @@
+(function() {
+	
+	var settings;
+	
+	return {
+		
+		settings: {
+			data: [
+				{"name": "Abrams Falls Trail", "id": 1, "length": 4.2, "description": "Highlights: Abrams Creek and Abrams Falls"},
+				{"name": "Ace Gap Trail", "id": 2, "length": 5.6, "description": ""},
+				{"name": "Albright Grove Loop Trail", "id": 3, "length": .7, "description": ""},
+				{"name": "Alum Cave Trail", "id": 4, "length": 5, "description": "From Newfound Gap Road to Rainbow Falls Trail near the summit of Mt. LeConte. Highlights: Arch Rock, Alum Cave Bluffs, old-growth forest, great views."},
+				{"name": "Anthony Creek Trail", "id": 5, "length": 3.6, "description": "From Cades Cove Picnic Area to Bote Mountain Trail. Highlights: Anthony Creek."},
+				{"name": "Appalachian Trail", "id": 6, "length": 71.4, "description": "The Appalachian Trail spans from Georgia to Maine, and 71.4 of its 2,200 miles are located in the Great Smokey Mountain National Park. It offers some of the toughest, most scenic, and most remote section of the park, following along the backbone of the rideline."},
+				{"name": "Balsam Mountain Trail", "id": 7, "length": 10.1, "description": ""},
+				{"name": "Gregory Bald Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Wolf Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Twenty Mile Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Twenty Mile Loop Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Wolf Ridge Trail 1 ", "id": 0, "length": 0, "description": ""},
+				{"name": "Lost Cove Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Lakeshore Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Tunnel Bypass", "id": 0, "length": 0, "description": ""},
+				{"name": "Goldmine Loop Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Ollie Cove Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Hazel Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Welch Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "High Rocks Trail 1 ", "id": 0, "length": 0, "description": ""},
+				{"name": "Bear Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Forney Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Jonas Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Springhouse Branch Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Noland Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Clingmans Dome Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Clingmans Dome Bypass Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Noland Divide Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Juney hank Falls Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Deep Creek Horse Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Deep Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Indian Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Martins Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Deeplow Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Sunkota Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Loop Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Stone Pile gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Indian Creek Motor Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Thomas Divide Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Mingus Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Fork Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Road Prong Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Chimney Tops Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Whiteoak Branch Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Hannah Mountain Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Rabbit Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Wet Bottom Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Gold Mine Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Cane Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Cooper Road Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Little Bottoms Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Hatcher Mountain Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Beard Cane Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Newton Bald Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Kanati Fork Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Pole Road Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Bone Valley Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Eagle Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Jenkins Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Cold Spring Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "High Rocks Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Gregory Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Long Hungry Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Hughes Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Dry Sluice Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Cabin Flats Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Bradley Fork Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Tow String Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Smokemont Loop Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Chasteen Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Grassy Branch Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Kephart Prong Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Rainbow falls Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Bull Head Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Old Sugarlands Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Trillium Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Brushy Mountain Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Grapeyard Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Baskins Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Twin Creeks Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Gatlinburg Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "The Boulevard Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Enloe Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Hyatt Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Beech Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Chestnut Bridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Big Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Camel Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Baxter Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Mt. Sterling Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Mt. sterling Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Long Bunk Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Palmer Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Little catalooche Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Pretty Hollow Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Flat Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Rough Fork Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Big Fork Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Caldwell Fork Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Boogerman Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "McKee Bridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Hemphill Bald Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Catalooche Divide Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Madron Bald Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Ramsey Cascades Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Porters Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Sweat Heifer Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Gunter Fork Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Low Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Gabes Mountain Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Old Settlers Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Snake Den Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Lower Mount Camerer Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Mt. Camerer Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Swallow Fork Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Indian Grave Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Rich Mountain Loop Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Crooked Arm Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Scott Mountain Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Chestnut Top Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Roundtop Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Little Greenbriar Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Russell Field Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Bote Mountain Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Schoolhouse Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Crib Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Finley Cane Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Turkey Pen Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "West Prong Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Lumber Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Meigs Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Meigs Mountain Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Curry Mountain Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Metcalf Bottoms Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Little Brier Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Laurel Falls Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Cove Mountain Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Greenbriar Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Lynn Camp Prong Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Middle Prong Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Panther Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Miry Ridge Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Jakes Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Cucumber Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Little River Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Rough Creek Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Sugarland Mountain Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Huskey Gap Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Goshen Prong Trail", "id": 0, "length": 0, "description": ""},
+				{"name": "Lead Cove Trail", "id": 0, "length": 0, "description": ""}
+			]
+		},
+		
+		init: function() {
+			
+			settings = this.settings;
+			
+			this.bindUI();
+		},
+		
+		bindUI: function() {
+			
+			$('svg#map').click(function(){
+				$(this).find('#Landscape').toggleClass('hidden');
+			});
+			
+			$('body').on('mouseenter', '.extended-hover-range', function() { // set up highlight on hover
+				var $parent = $(this).closest('g');
+				var $trail = $parent.find('.st0');
+				$trail.css('stroke', 'orange');
+				$('.info .trail-name').text(utils.formatID($parent.attr('id'))); // replace underscores in ID to spaces
+			});
+
+			$('body').on('mouseleave', '.extended-hover-range', function() {
+				$(this).closest('g').find('.st0').css('stroke', '');
+			});
+			
+			var trails = [];
+			$('svg g').each(function(i) {
+				trails.push({
+					"name": utils.formatID($(this).attr('id')),
+					"length": 0,
+					"description": ""
+				}); 
+			});
+			
+			console.log(trails)
+			
+			//create clones of trails that can be invisible in order to trigger larger areas
+			$('svg#map').find('.st0').each(function() {
+				var $parent = $(this).closest('g');
+				var $original = $(this);
+				var $copy = $(this).clone();
+				$copy.css({
+					'stroke': 'transparent',
+					'fill': 'none',
+					'stroke-width': 40,
+					'position': 'relative',
+					'z-index': '9999'
+				});
+				$parent.prepend($copy);
+				$copy.removeAttr('class').addClass('extended-hover-range');
+			});
+		}
+	};
+}());
