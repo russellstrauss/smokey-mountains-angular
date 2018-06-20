616014c851898ccefdb25701eaba2f750bef1bef
Russell Strauss <rstrauss@agencyoasis.com>
2018-06-19 21:54:27 -0400

    Trying to interact with dom

--- ng5/src/app/app.trail-map.ts
+++ ng5/src/app/app.trail-map.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ngOnInit, AfterViewInit } from '@angular/core';
+
+declare function bindUI();
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.trail-map.html',
-  styleUrls: ['./app.trail-map.scss']
+	selector: 'app-root',
+	templateUrl: './app.trail-map.html',
+	styleUrls: ['./app.trail-map.scss']
 })
 
-export class AppComponent {
-  title = 'Smokey Mountain Interactive Trail Map';
+export class AppComponent implements AfterViewInit {
+	title = 'Smokey Mountain Interactive Trail Map';
+	
+	ngOnInit {
+	}
 }
